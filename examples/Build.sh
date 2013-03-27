6fcf5da2a3ea4ba667d441c5897143c55a1e69a9
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2013-03-27 20:48:24 +0100

    New examples: clonesrv1, clonecli1, kvsimple

--- examples/Build.sh
+++ examples/Build.sh
@@ -3,6 +3,7 @@
 go install github.com/pebbe/zmq2/examples/bstar
 go install github.com/pebbe/zmq2/examples/mdapi
 go install github.com/pebbe/zmq2/examples/flcliapi
+go install github.com/pebbe/zmq2/examples/kvsimple
 
 go get code.google.com/p/go-uuid/uuid
 
