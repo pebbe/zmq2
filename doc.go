60192c055fe873f755ee99be78e718121a3f69dd
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2014-06-24 17:20:27 +0200

    Missing file doc.go added.

---
+++ doc.go
@@ -0,0 +1,24 @@
+/*
+A Go interface to ZeroMQ (zmq, 0mq) version 2.
+
+For ZeroMQ version 4, see: http://github.com/pebbe/zmq4
+
+For ZeroMQ version 3, see: http://github.com/pebbe/zmq3
+
+Requires ZeroMQ version 2.1 or 2.2
+
+The following functions return ErrorNotImplemented in 0MQ version 2.1:
+
+(*Socket)GetRcvtimeo, (*Socket)GetSndtimeo, (*Socket)SetRcvtimeo, (*Socket)SetSndtimeo
+
+http://www.zeromq.org/
+
+A note on the use of a context:
+
+This package provides a default context. This is what will be used by
+the functions without a context receiver, that create a socket or
+manipulate the context. Package developers that import this package
+should probably not use the default context with its associated
+functions, but create their own context(s). See: type Context.
+*/
+package zmq2
