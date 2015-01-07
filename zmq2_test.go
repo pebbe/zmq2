7e56eefb13184562ace425232c3e68924b2a75fd
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-01-07 17:30:40 +0100

    new test: version check

--- zmq2_test.go
+++ zmq2_test.go
@@ -1,78 +1,71 @@
 package zmq2_test
 
 import (
-	//zmq "github.com/pebbe/zmq2"
+	zmq "github.com/pebbe/zmq2"
 
 	"fmt"
 	"runtime"
 )
 
+func Example_version() {
+	major, minor, _ := zmq.Version()
+	fmt.Println("Version major:", major)
+	fmt.Println("Version minor == 1|2:", minor == 1 || minor == 2)
+	// Output:
+	// Version major: 2
+	// Version minor == 1|2: true
+}
 
 func Example_test_hwm() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_pair_inproc() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_pair_ipc() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_pair_tcp() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_reqrep_inproc() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_reqrep_ipc() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
-
 
 func Example_test_reqrep_tcp() {
 
-
 	fmt.Println("Done")
 	// Output:
 	// Done
 }
 
-
 func Example_test_shutdown_stress() {
-
 
 	fmt.Println("Done")
 	// Output:
