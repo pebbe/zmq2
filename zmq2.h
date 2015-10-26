70f71c462e69cbb21b0246aa4da320ea9c76d2c8
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-10-26 11:05:01 +0100

    Compile time version check

--- zmq2.h
+++ zmq2.h
@@ -1,7 +1,9 @@
+#if ZMQ_VERSION_MAJOR != 2
+#error You need ZeroMQ version 2 to build this
+#endif
 #ifndef ZMQ_RCVTIMEO
 #define ZMQ_RCVTIMEO 0
 #endif
 #ifndef ZMQ_SNDTIMEO
 #define ZMQ_SNDTIMEO 0
 #endif
-
