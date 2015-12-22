1b54b470300a94005ac634fb3aa75e9f1ab9ad16
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-12-22 18:01:05 +0100

    Replaced example by test

--- zmq2_test.go
+++ zmq2_test.go
@@ -3,84 +3,12 @@ package zmq2_test
 import (
 	zmq "github.com/pebbe/zmq2"
 
-	"fmt"
-	"runtime"
+	"testing"
 )
 
-func Example_test_version() {
-	major, minor, _ := zmq.Version()
-	fmt.Println("Version major:", major)
-	fmt.Println("Version minor == 1|2:", minor == 1 || minor == 2)
-	// Output:
-	// Version major: 2
-	// Version minor == 1|2: true
-}
-
-func Example_test_hwm() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_inproc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_ipc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_tcp() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_inproc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_ipc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_tcp() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_shutdown_stress() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func checkErr(err error) bool {
-	if err != nil {
-		_, filename, lineno, ok := runtime.Caller(1)
-		if ok {
-			fmt.Printf("%v:%v: %v\n", filename, lineno, err)
-		} else {
-			fmt.Println(err)
-		}
-		return true
+func TestVersion(t *testing.T) {
+	major, _, _ := zmq.Version()
+	if major != 2 {
+		t.Errorf("Expected major version 2, got %d", major)
 	}
-	return false
 }
